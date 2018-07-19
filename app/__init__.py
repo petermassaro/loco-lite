f4acd28eb8a53e186f85c8dd97099744809f49f3
Peter Massaro <petermassaro@Peters-Air.home>
2018-07-19 16:28:11 -0400

    Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from flask_bootstrap import Bootstrap
 from config import Config
 import boto3
 from flask_moment import Moment
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 
 db = SQLAlchemy()
@@ -42,6 +44,41 @@ def create_app(config_class=Config):
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
+
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'],
+                        app.config['MAIL_PASSWORD'])
+            secure = None
+            # if app.config['MAIL_USE_TLS']:
+            #     secure = ()
+            # # mail_handler = SMTPHandler(
+            # #     mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            # #     fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            # #     toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            # #     credentials=auth, secure=secure)
+            # # mail_handler.setLevel(logging.ERROR)
+            # app.logger.addHandler(mail_handler)
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/loco-lite.log',
+                                               maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('loco-lite startup')
 
 
     return app
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
     QRS_PER_PAGE = 10
 
